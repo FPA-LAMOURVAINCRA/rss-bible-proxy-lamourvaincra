48619e614012945a78c06e9deff127bc64bf445f
FPA-LAMOURVAINCRA <contact.lamourvaincra@gmail.com>
2025-03-25 19:15:44 +0100

    Update rss.js

--- api/rss.js
+++ api/rss.js
@@ -1,5 +1,5 @@
 export default async function handler(req, res) {
-  const response = await fetch('https://feeds.podcastics.com/podcastics/podcasts/rss/7543_22adb373093deb54e1ec644c2a7adec7.rss');
+  const response = await fetch('https://feed.ausha.co/YPwzEUG09qYN');
   const data = await response.text();
 
   res.setHeader('Content-Type', 'application/xml');
