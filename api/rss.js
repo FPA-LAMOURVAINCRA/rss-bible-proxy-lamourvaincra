1f49f8dda949b0b1f51040bc22d0451dbdc65b57
FPA-LAMOURVAINCRA <contact.lamourvaincra@gmail.com>
2025-03-25 18:56:54 +0100

    Update rss.js

--- api/rss.js
+++ api/rss.js
@@ -1,3 +1,7 @@
-export default function handler(req, res) {
-  res.status(200).json({ message: "Tout fonctionne 🎉" });
+export default async function handler(req, res) {
+  const response = await fetch('https://www.podcastics.com/podcast/rss/7543');
+  const data = await response.text();
+
+  res.setHeader('Content-Type', 'application/xml');
+  res.status(200).send(data);
 }
